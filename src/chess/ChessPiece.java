7bed70d454498adaf38a75150706b6abdea8c637
Albert Fortunato <f.ortunato@hotmail.com>
2021-10-25 13:13:53 -0300

    Implementando movimentos possiveis da torre

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,9 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
+
+import javax.management.openmbean.CompositeDataSupport;
 
 public abstract class ChessPiece extends Piece {
 
@@ -14,5 +17,9 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+    protected boolean isThereOpponentPiece (Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 }
